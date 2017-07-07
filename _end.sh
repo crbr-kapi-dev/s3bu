45b16238ffe9ad256ca246112ae4cd377409438e
Adrian Esquivel <adrianesquivels@gmail.com>
2017-07-07 15:18:18 -0600

    fix: file not found

--- _end.sh
+++ _end.sh
@@ -2,7 +2,7 @@
 s3_file_exists $S3BUCKET $FILENAME.tar.gz
 
 #copy to s3
-s3_save_file $OUTPUT$FILENAME.tar.gz $S3BUCKET
+s3_save_file $S3BUCKET $OUTPUT$FILENAME.tar.gz
 echo "S3 PUT to bucket: $S3BUCKET"
 
 if [ $DEL_TEMPDIR == 1 ]; then
